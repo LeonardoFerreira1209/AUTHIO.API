19797a52105332b75ed88744eda5bec280eb6425
Leonardo Ferreira de Almeida <Leo.Ferreira30@outlook.com>
2024-08-24 01:21:29 -0300

    Feature/feature ajuste libs (#76)
    * Subindo ajustes nas libs.
    
    * Subindo ajustes nas libs.
    
    * Subindo ajustes na aplicação tais como, nova classe de filtros e ajustes em namespaces
    
    * Subindo ajustes na aplicação.
    
    * Subindo ajustes
    
    * Subindo ajustes
    
    * Subindo ajustes na configuração de auth, deixando o events de jwt em uma classe.
    
    * Subindo ajustes na aplicação.
    
    * Subindo ajustes na aplicação subindo integração de criptografia de tokens, baseada na lib NetDevPackSecurityJwt.
    
    * Subindo ajustes
    
    * Ajustes
    
    * Ajustes
    
    * Adicionado ClockSkew
    
    * Ajuste
    
    * Ajustes horas.
    
    * Subindo ajustes.
    
    * Subindo novos ajustes em gerenciamentos de chaves de token mult tenant e controle de cahces.
    
    * Subindo ajustes
    
    * Subindo ajustes

--- src/AUTHIO.DOMAIN/Contracts/Jwt/IJwtService.cs
+++ src/AUTHIO.DOMAIN/Contracts/Jwt/IJwtService.cs
@@ -4,18 +4,53 @@ using System.Collections.ObjectModel;
 
 namespace AUTHIO.DOMAIN.Contracts.Jwt;
 
+/// <summary>
+/// Interface da service do JWT.
+/// </summary>
 public interface IJwtService
 {
     /// <summary>
-    /// By default it will use JWS options to create a Key if doesn't exist
-    /// If you want to use JWE, you must select RSA. Or use `CryptographicKey` class
+    /// Gerar uma chave.
     /// </summary>
     /// <returns></returns>
     Task<SecurityKey> GenerateKey();
+
+    /// <summary>
+    /// Buscar chave de segurança atual.
+    /// </summary>
+    /// <returns></returns>
     Task<SecurityKey> GetCurrentSecurityKey();
+
+    /// <summary>
+    /// Buscar credencial de assinatura atual.
+    /// </summary>
+    /// <returns></returns>
     Task<SigningCredentials> GetCurrentSigningCredentials();
+
+    /// <summary>
+    /// Buscar credencial encripitada atual.
+    /// </summary>
+    /// <returns></returns>
     Task<EncryptingCredentials> GetCurrentEncryptingCredentials();
+
+    /// <summary>
+    /// Buscar ultima chave.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
     Task<ReadOnlyCollection<KeyMaterial>> GetLastKeys(int? i = null);
+
+    /// <summary>
+    /// Revogar chave.
+    /// </summary>
+    /// <param name="keyId"></param>
+    /// <param name="reason"></param>
+    /// <returns></returns>
     Task RevokeKey(string keyId, string reason = null);
+
+    /// <summary>
+    /// Gerar nova chave.
+    /// </summary>
+    /// <returns></returns>
     Task<SecurityKey> GenerateNewKey();
 }
--- src/AUTHIO.INFRASTRUCTURE/Services/JwtService.cs
+++ src/AUTHIO.INFRASTRUCTURE/Services/JwtService.cs
@@ -7,142 +7,149 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.Collections.ObjectModel;
 
-namespace AUTHIO.INFRASTRUCTURE.Services
+namespace AUTHIO.INFRASTRUCTURE.Services;
+
+/// <summary>
+/// Service de Jwt.
+/// </summary>
+/// <param name="store"></param>
+/// <param name="cryptoService"></param>
+/// <param name="contextService"></param>
+/// <param name="options"></param>
+/// <param name="tenantRepository"></param>
+public class JwtService(
+    IJsonWebKeyStore store, 
+    ICryptoService cryptoService, 
+    IContextService contextService,
+    IOptions<JwtOptions> options,
+    ITenantRepository tenantRepository) : IJwtService
 {
-    public class JwtService(
-        IJsonWebKeyStore store, 
-        ICryptoService cryptoService, 
-        IContextService contextService,
-        IOptions<JwtOptions> options,
-        ITenantRepository tenantRepository) : IJwtService
+    private readonly IJsonWebKeyStore _store = store;
+    private readonly IOptions<JwtOptions> _options = options;
+
+    private readonly Guid? _currentTenantId
+      = tenantRepository.GetAsync(
+          x => x.TenantConfiguration.TenantKey == contextService.GetCurrentTenantKey())?.Result?.Id;
+
+    /// <summary>
+    /// Gerar chave.
+    /// </summary>
+    /// <returns></returns>
+    public async Task<SecurityKey> GenerateKey()
     {
-        private readonly IJsonWebKeyStore _store = store;
-        private readonly IOptions<JwtOptions> _options = options;
+        var key = new CryptographicKey(cryptoService, _options.Value.Jws);
 
-        private readonly Guid? _currentTenantId
-          = tenantRepository.GetAsync(
-              x => x.TenantConfiguration.TenantKey == contextService.GetCurrentTenantKey())?.Result?.Id;
+        var model = new KeyMaterial(key, _currentTenantId);
+        await _store.Store(model);
 
-        /// <summary>
-        /// Gerar chave.
-        /// </summary>
-        /// <returns></returns>
-        public async Task<SecurityKey> GenerateKey()
+        return model.GetSecurityKey();
+    }
+
+    /// <summary>
+    /// Recuperar chave de segurança atual.
+    /// </summary>
+    /// <returns></returns>
+    public async Task<SecurityKey> GetCurrentSecurityKey()
+    {
+        var current = await _store.GetCurrent();
+
+        if (NeedsUpdate(current))
         {
-            var key = new CryptographicKey(cryptoService, _options.Value.Jws);
-
-            var model = new KeyMaterial(key, _currentTenantId);
-            await _store.Store(model);
-
-            return model.GetSecurityKey();
+            // According NIST - https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-57pt1r4.pdf - Private key should be removed when no longer needs
+            await _store.Revoke(current);
+            var newKey = await GenerateKey();
+            return newKey;
         }
 
-        /// <summary>
-        /// Recuperar chave de segurança atual.
-        /// </summary>
-        /// <returns></returns>
-        public async Task<SecurityKey> GetCurrentSecurityKey()
+        // options has change. Change current key
+        if (!await CheckCompatibility(current))
+            current = await _store.GetCurrent();
+
+        return current;
+    }
+
+    /// <summary>
+    /// Recupera credenciais assinada atuais.
+    /// </summary>
+    /// <returns></returns>
+    public async Task<SigningCredentials> GetCurrentSigningCredentials()
+    {
+        var current = await GetCurrentSecurityKey();
+
+        return new SigningCredentials(current, _options.Value.Jws);
+    }
+
+    /// <summary>
+    /// Recupera credenciais assinada encripitadas atuais.
+    /// </summary>
+    /// <returns></returns>
+    public async Task<EncryptingCredentials> GetCurrentEncryptingCredentials()
+    {
+        var current = await GetCurrentSecurityKey();
+
+        return new EncryptingCredentials(current, _options.Value.Jwe.Alg, _options.Value.Jwe.EncryptionAlgorithmContent);
+    }
+
+    /// <summary>
+    /// Recuperar ultimas chaves.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
+    public Task<ReadOnlyCollection<KeyMaterial>> GetLastKeys(int? i = null)
+    {
+        return _store.GetLastKeys(_options.Value.AlgorithmsToKeep);
+    }
+
+    /// <summary>
+    /// Recupear compatibilidade.
+    /// </summary>
+    /// <param name="currentKey"></param>
+    /// <returns></returns>
+    private async Task<bool> CheckCompatibility(KeyMaterial currentKey)
+    {
+        if (currentKey.Type != _options.Value.Jws.Kty())
         {
-            var current = await _store.GetCurrent();
-
-            if (NeedsUpdate(current))
-            {
-                // According NIST - https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-57pt1r4.pdf - Private key should be removed when no longer needs
-                await _store.Revoke(current);
-                var newKey = await GenerateKey();
-                return newKey;
-            }
-
-            // options has change. Change current key
-            if (!await CheckCompatibility(current))
-                current = await _store.GetCurrent();
-
-            return current;
+            await GenerateKey();
+            return false;
         }
+        return true;
+    }
 
-        /// <summary>
-        /// Recupera credenciais assinada atuais.
-        /// </summary>
-        /// <returns></returns>
-        public async Task<SigningCredentials> GetCurrentSigningCredentials()
-        {
-            var current = await GetCurrentSecurityKey();
+    /// <summary>
+    /// Revogar chave.
+    /// </summary>
+    /// <param name="keyId"></param>
+    /// <param name="reason"></param>
+    /// <returns></returns>
+    public async Task RevokeKey(string keyId, string reason = null)
+    {
+        var key = await _store.Get(keyId);
 
-            return new SigningCredentials(current, _options.Value.Jws);
-        }
+        await _store.Revoke(key, reason);
+    }
 
-        /// <summary>
-        /// Recupera credenciais assinada encripitadas atuais.
-        /// </summary>
-        /// <returns></returns>
-        public async Task<EncryptingCredentials> GetCurrentEncryptingCredentials()
-        {
-            var current = await GetCurrentSecurityKey();
+    /// <summary>
+    /// Gerar nova chave.
+    /// </summary>
+    /// <returns></returns>
+    public async Task<SecurityKey> GenerateNewKey()
+    {
+        var oldCurrent = await _store.GetCurrent();
 
-            return new EncryptingCredentials(current, _options.Value.Jwe.Alg, _options.Value.Jwe.EncryptionAlgorithmContent);
-        }
+        await _store.Revoke(oldCurrent);
 
-        /// <summary>
-        /// Recuperar ultimas chaves.
-        /// </summary>
-        /// <param name="i"></param>
-        /// <returns></returns>
-        public Task<ReadOnlyCollection<KeyMaterial>> GetLastKeys(int? i = null)
-        {
-            return _store.GetLastKeys(_options.Value.AlgorithmsToKeep);
-        }
+        return await GenerateKey();
+    }
 
-        /// <summary>
-        /// Recupear compatibilidade.
-        /// </summary>
-        /// <param name="currentKey"></param>
-        /// <returns></returns>
-        private async Task<bool> CheckCompatibility(KeyMaterial currentKey)
-        {
-            if (currentKey.Type != _options.Value.Jws.Kty())
-            {
-                await GenerateKey();
-                return false;
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Revogar chave.
-        /// </summary>
-        /// <param name="keyId"></param>
-        /// <param name="reason"></param>
-        /// <returns></returns>
-        public async Task RevokeKey(string keyId, string reason = null)
-        {
-            var key = await _store.Get(keyId);
-
-            await _store.Revoke(key, reason);
-        }
-
-        /// <summary>
-        /// Gerar nova chave.
-        /// </summary>
-        /// <returns></returns>
-        public async Task<SecurityKey> GenerateNewKey()
-        {
-            var oldCurrent = await _store.GetCurrent();
-
-            await _store.Revoke(oldCurrent);
-
-            return await GenerateKey();
-        }
-
-        /// <summary>
-        /// Precisa de atualização ?.
-        /// </summary>
-        /// <param name="current"></param>
-        /// <returns></returns>
-        private bool NeedsUpdate(KeyMaterial current)
-        {
-            return current == null
-                || current.IsExpired(_options.Value.DaysUntilExpire) 
-                || current.IsRevoked;
-        }
+    /// <summary>
+    /// Precisa de atualização ?.
+    /// </summary>
+    /// <param name="current"></param>
+    /// <returns></returns>
+    private bool NeedsUpdate(KeyMaterial current)
+    {
+        return current == null
+            || current.IsExpired(_options.Value.DaysUntilExpire) 
+            || current.IsRevoked;
     }
 }
