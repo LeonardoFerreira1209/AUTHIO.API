22dabd07080fd6f26dcbe017c5fd65a031e1e092
Leonardo Almeida <Leo.Ferreira30@outlook.com>
2024-10-11 02:10:28 -0300

    Ajustes

---
+++ src/AUTHIO.DOMAIN/Dtos/Response/PlanResponse.cs
@@ -0,0 +1,64 @@
+﻿using AUTHIO.DOMAIN.Enums;
+
+namespace AUTHIO.DOMAIN.Dtos.Response;
+
+/// <summary>
+/// Classe de response de plano.
+/// </summary>
+public class PlanResponse
+{
+    /// <summary>
+    /// Id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Data de criação.
+    /// </summary>
+    public DateTime Created { get; set; }
+
+    /// <summary>
+    /// Data de atualização.
+    /// </summary>
+    public DateTime? Updated { get; set; } = null;
+
+    /// <summary>
+    /// Status.
+    /// </summary>
+    public Status Status { get; set; }
+
+    /// <summary>
+    /// Nome do plano.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Descrição do plano.
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Valor do plano.
+    /// </summary>
+    public decimal Value { get; set; }
+
+    /// <summary>
+    /// Pagamento mensal.
+    /// </summary>
+    public bool MonthlyPayment { get; set; }
+
+    /// <summary>
+    /// Quantidade de tenants liberado para cadastro.
+    /// </summary>
+    public int QuantTenants { get; set; }
+
+    /// <summary>
+    /// Quantidade de usuários liberado para cadastro em cada tenant.
+    /// </summary>
+    public int QuantUsers { get; set; }
+
+    /// <summary>
+    /// Coleção de usuários vinculados ao plano.
+    /// </summary>
+    public ICollection<UserResponse> Users { get; set; }
+}
---
+++ src/AUTHIO.DOMAIN/Helpers/Extensions/PlanExtensions.cs
@@ -0,0 +1,34 @@
+﻿using AUTHIO.DOMAIN.Dtos.Response;
+using AUTHIO.DOMAIN.Entities;
+
+namespace AUTHIO.DOMAIN.Helpers.Extensions;
+
+/// <summary>
+/// Classe de extensão de planos.
+/// </summary>
+public static class PlanExtensions
+{
+    /// <summary>
+    /// Entity to response.
+    /// </summary>
+    /// <param name="plan"></param>
+    /// <returns></returns>
+    public static PlanResponse ToResponse(
+        this PlanEntity plan, 
+        bool includeUsers = true) => new()
+        {
+            Created = plan.Created,
+            Description = plan.Description,
+            Id = plan.Id,
+            MonthlyPayment = plan.MonthlyPayment,
+            Name = plan.Name,
+            QuantTenants = plan.QuantTenants,
+            QuantUsers = plan.QuantUsers,
+            Status = plan.Status,
+            Updated = plan.Updated,
+            Value = plan.Value,
+            Users = includeUsers 
+                ? plan.Users.Select(u => u.ToResponse()).ToList() 
+                : null  
+        };
+}
